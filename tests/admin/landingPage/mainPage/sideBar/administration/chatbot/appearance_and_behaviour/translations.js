705543715a0e615287e0ed6acc7edd73cb947320
mmuuripe <markus.muuripeal@gmail.com>
2024-09-03 14:43:57 +0300

    testing the universal approach to english/estonian page with translations file

---
+++ tests/admin/landingPage/mainPage/sideBar/administration/chatbot/appearance_and_behaviour/translations.js
@@ -0,0 +1,24 @@
+export const translations = {
+    en: {
+        'Animatsiooni kestus sekundites': 'Widget proactive seconds',
+        'Märguandesõnum': 'Widget bubble message',
+        'Aeg animatsiooni alguseni sekundites': 'Widget bubble message seconds',
+        'Märguandesõnum': 'Widget bubble message',
+        'Põhivärv': 'Widget color',
+        'Animatsioon': 'Widget animation',
+        'Välimus ja käitumine': 'Appearance and behaviour',
+        'Salvesta': 'Save',
+        'Eelvaade': 'Preview',
+    },
+    et: {
+        'Animatsiooni kestus sekundites': 'Animatsiooni kestus sekundites',
+        'Märguandesõnum': 'Märguandesõnum',
+        'Aeg animatsiooni alguseni sekundites': 'Aeg animatsiooni alguseni sekundites',
+        'Märguandesõnum': 'Märguandesõnum',
+        'Põhivärv': 'Põhivärv',
+        'Animatsioon': 'Animatsioon',
+        'Välimus ja käitumine': 'Välimus ja käitumine',
+        'Salvesta': 'Salvesta',
+        'Eelvaade': 'Eelvaade',
+    }
+};
