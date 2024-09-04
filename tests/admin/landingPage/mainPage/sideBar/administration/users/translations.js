5b77333b5537e0fc4f1633a96011cc72d69a95a8
mmuuripe <markus.muuripeal@gmail.com>
2024-09-04 12:28:23 +0300

    users translations est/eng

---
+++ tests/admin/landingPage/mainPage/sideBar/administration/users/translations.js
@@ -0,0 +1,26 @@
+export const translations = {
+    en: {
+        'Kasutajad': 'Users',
+        'Lisa kasutaja': 'Add User',
+        'Nimi': 'Name',
+        'Isikukood': 'ID code',
+        'Roll': 'Role',
+        'Kuvatav nimi': 'Display Name',
+        'Tiitel': 'User title',
+        'E-post': 'E-mail',
+        'Muuda': 'Edit',
+        'Kustuta': 'Delete',
+    },
+    et: {
+        'Kasutajad': 'Kasutajad',
+        'Lisa kasutaja': 'Lisa kasutaja',
+        'Nimi': 'Nimi',
+        'Isikukood': 'Isikukood',
+        'Roll': 'Roll',
+        'Kuvatav nimi': 'Kuvatav nimi',
+        'Tiitel': 'Tiitel',
+        'E-post': 'E-post',
+        'Muuda': 'Muuda',
+        'Kustuta': 'Kustuta',
+    }
+};
