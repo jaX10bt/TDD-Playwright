ef7330fd4bcbfe807167f441589d22cb46ba0af1
T4NKER <markus.muuripeal@gmail.com>
2024-09-10 14:07:29 +0300

    updated appearance and behavior tests

--- tests/admin/landingPage/mainPage/sideBar/administration/chatbot/appearance_and_behavior/visbility.spec.js
+++ tests/admin/landingPage/mainPage/sideBar/administration/chatbot/appearance_and_behavior/visbility.spec.js
@@ -2,61 +2,60 @@ import { test, expect } from '@playwright/test';
 import { getTranslations } from '../../../../../../../translations/languageDetector';
 
 test.describe('Visibility Tests for "Välimus ja käitumine"/"Appearance and Behaviour" Page', () => {
-
-    const translation = getTranslations('https://admin.test.buerokratt.ee/chat/chatbot/appearance');
-
+    let translation
     test.beforeEach(async ({ page }) => {
         // Navigate to the page before each test
-        await page.goto('https://admin.test.buerokratt.ee/chat/chatbot/appearance');
+        await page.goto('https://admin.prod.buerokratt.ee/chat/chatbot/appearance');
+        translation = await getTranslations(page);
     });
 
     test('Check visibility of the header', async ({ page }) => {
-        const header = page.locator(`h1:has-text("${translation['AppearanceAndBehvaior']}")`);
+        const header = page.locator(`h1:has-text("${translation['appearanceAndBehaviour']}")`);
         await expect(header).toBeVisible();
     });
 
     test('Check visibility of animation duration input', async ({ page }) => {
-        const animationDurationInput = page.locator(`label:has-text("${translation['WidgetProactiveSeconds']}") + div input`);
+        const animationDurationInput = page.locator(`label:has-text("${translation['widgetProactiveSeconds']}") + div input`);
         await expect(animationDurationInput).toBeVisible();
     });
 
     test('Check visibility of notification switch', async ({ page }) => {
-        const notificationSwitch = page.locator(`label:has-text("${translation['WidgetBubbleMessage']}") + button.switch__button`);
+        const notificationSwitch = page.locator(`label:has-text("${translation['widgetBubbleMessage']}") + button.switch__button`);
         await expect(notificationSwitch).toBeVisible();
     });
 
     test('Check visibility of animation start time input', async ({ page }) => {
-        const animationStartTimeInput = page.locator(`label:has-text("${translation['WidgetBubbleMessageSeconds']}") + div input`);
+        const animationStartTimeInput = page.locator(`label:has-text("${translation['widgetBubbleMessageSeconds']}") + div input`);
         await expect(animationStartTimeInput).toBeVisible();
     });
 
     test('Check visibility of notification message input', async ({ page }) => {
-        const notificationMessageInput = page.locator(`label:has-text("${translation['WidgetBubbleMessage']}") + div input`);
+        const notificationMessageInput = page.locator(`label:has-text("${translation['widgetBubbleMessage']}") + div input`);
         await expect(notificationMessageInput).toBeVisible();
     });
 
     test('Check visibility of primary color picker', async ({ page }) => {
-        const primaryColorPicker = page.locator(`label:has-text("${translation['WidgetColor']}") + div input`);
+        const primaryColorPicker = page.locator(`label:has-text("${translation['widgetColor']}") + div input`);
         await expect(primaryColorPicker).toBeVisible();
     });
 
     test('Check visibility of color picker button', async ({ page }) => {
-        const colorPickerButton = page.locator(`label:has-text("${translation['WidgetColor']}") + div input`);
+        const colorPickerButton = page.locator(`label:has-text("${translation['widgetColor']}") + div input`);
         await expect(colorPickerButton).toBeVisible();
     });
 
     test('Check visibility of animation dropdown', async ({ page }) => {
-        const animationDropdown = page.locator(`label:has-text("${translation['WidgetAnimation']}") + div div.select__trigger`);
+        const animationDropdown = page.locator(`label:has-text("${translation['widgetAnimation']}") + div div.select__trigger`);
         await expect(animationDropdown).toBeVisible();
     });
 
     test('Check visibility of save button', async ({ page }) => {
-        const saveButton = page.locator(`button:has-text("${translation['Save']}")`);
+        const saveButton = page.locator(`button:has-text("${translation['save']}")`);
         await expect(saveButton).toBeVisible();
     });
 
     test('Check visibility of preview button', async ({ page }) => {
-        const previewButton = page.locator(`button:has-text("${translation['Preview']}")`);
+        const previewButton = page.locator(`button:has-text("${translation['preview']}")`);
         await expect(previewButton).toBeVisible();
     });
 
