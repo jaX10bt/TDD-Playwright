c606c6eaea7e4dfff9e33bb678a476cc27c421d7
danyil <danyil.kurbatov@gmail.com>
2024-09-27 18:11:32 +0300

    While conversion from business dsl to test dsl add the title part

--- tests/DSL/DSLConverter/DSLConverter.js
+++ tests/DSL/DSLConverter/DSLConverter.js
@@ -44,6 +44,7 @@ class DSLConverter {
     }
   }
 
+  // Function to convert businessDSL to testDSL. Go through each method and generate a test.
   convertToTestDSL() {
     if (!this.businessDSL) {
       console.error("Business DSL not loaded.");
@@ -53,6 +54,12 @@ class DSLConverter {
     let testDSL = '';
 
     this.businessDSL.methods.forEach(method => {
+
+      // TODO: maybe can combine with generateTest later...
+      if (method.main && method.main.title) {
+        testDSL += this.generateTitleTest(method.main.title);
+      }
+
       testDSL += this.generateTest(method);
     });
 
@@ -61,6 +68,8 @@ class DSLConverter {
     return testDSL;
   }
 
+
+  // Function to generate a test for a body component.
   generateTest(method) {
     let testTemplate = '';
 
@@ -81,6 +90,45 @@ class DSLConverter {
     }
 
     return testTemplate;
+  }
+
+  // Specificly
+  generateTitleTest(title) {
+    let titleTestTemplate = '';
+    const titleData = title.components.title.args || [];
+
+    // Get the type of component. title.components is a dictionary. Object.keys returns list of all keys. 
+    //In case that title.components has one key which is 'title' should get from list first value
+    let componentType = Object.keys(title.components)[0];
+    let componentTemplate = this.templates[componentType];
+
+    if (componentTemplate) {
+      titleTestTemplate += this.populateTemplateWithTitle(componentTemplate, titleData);
+    }
+
+    return titleTestTemplate;
+  }
+
+  populateTemplateWithTitle(template, titleData) {
+    // Extract type, value, and style from the titleData array
+    const typeObj = titleData.find(item => item.type);
+    const valueObj = titleData.find(item => item.value);
+    const styleObj = titleData.find(item => item.style);
+
+    
+    // If any of the extracted values are undefined, set them to an empty string
+    const type = typeObj ? typeObj.type : '';
+    const value = valueObj ? this.toCamelCase(valueObj.value) : '';
+    const style = styleObj ? styleObj.style : 'h1';
+
+
+
+    // Replace placeholders in the title template with extracted values
+    const populatedTemplate = template
+      .replace(/{{\s*type\s*}}/g, type)
+      .replace(/{{\s*header\s*}}/g, `translation.${value}`)
+      .replace(/{{\s*style\s*}}/g, style);
+    return populatedTemplate;
   }
 
   populateTemplate(template, component) {
@@ -117,30 +165,30 @@ class DSLConverter {
       .replace(/\s+/g, '');
   }
 
-cleanTestDSL(testDSL) {
-	// On top of the file is the 'name'
-	let name = `name: ${this.businessDSL.description}\n`;
+  cleanTestDSL(testDSL) {
+    // On top of the file is the 'name'
+    let name = `name: ${this.businessDSL.description}\n`;
 
-	// Construct the dynamic navigation URL
-	let navigateUrl = `${this.baseUrl}${this.businessDSL.resource}`;
-	
-	// Setup block with tabs for indentation
-	let setupBlock = `setup:\n\t- describe: Check visibility of Page Elements\n\t  serial: true\n\t  beforeEach:\n\t  - name: Setup\n\t\t  action:\n\t\t\t  navigate: "${navigateUrl}"\n\t  - name: Fetch Translations\n\t\t\t  action:\n\t\t\t\t  getTranslations: true\n\t\t\t\t  assignVariable: translations\n`;
+    // Construct the dynamic navigation URL
+    let navigateUrl = `${this.baseUrl}${this.businessDSL.resource}`;
 
-	// Add tests block
-	let testsLabel = 'tests:\n';
+    // Setup block with tabs for indentation
+    let setupBlock = `setup:\n\t- describe: Check visibility of Page Elements\n\t  serial: true\n\t  beforeEach:\n\t  - name: Setup\n\t\t  action:\n\t\t\t  navigate: "${navigateUrl}"\n\t  - name: Fetch Translations\n\t\t\t  action:\n\t\t\t\t  getTranslations: true\n\t\t\t\t  assignVariable: translations\n`;
 
-  // Indent the entire testDSL block by adding a tab in front of each line
-	testDSL = testDSL.replace(/^/gm, '\t');
+    // Add tests block
+    let testsLabel = 'tests:\n';
 
-	// Clean up 'templates:' if present in the testDSL
-	testDSL = testDSL.replace(/templates:\s*/g, '');
+    // Indent the entire testDSL block by adding a tab in front of each line
+    testDSL = testDSL.replace(/^/gm, '\t');
 
-	// Combine all parts: name, setup block, and tests
-	testDSL = name + setupBlock + testsLabel + testDSL;
+    // Clean up 'templates:' if present in the testDSL
+    testDSL = testDSL.replace(/templates:\s*/g, '');
 
-	return testDSL;
-}
+    // Combine all parts: name, setup block, and tests
+    testDSL = name + setupBlock + testsLabel + testDSL;
+
+    return testDSL;
+  }
 }
 
 const dslConverter = new DSLConverter();
