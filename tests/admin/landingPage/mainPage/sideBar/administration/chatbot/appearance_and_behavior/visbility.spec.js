53388c3b595ab957c65ee49c7944a4df91c612dc
danyil <danyil.kurbatov@gmail.com>
2024-10-01 13:37:58 +0300

    Added jsconfig where configured translations file path, so import is not relying on relative path.

--- tests/admin/landingPage/mainPage/sideBar/administration/chatbot/appearance_and_behavior/visbility.spec.js
+++ tests/admin/landingPage/mainPage/sideBar/administration/chatbot/appearance_and_behavior/visbility.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { getTranslations } from '../../../../../../../translations/languageDetector';
+import { getTranslations } from '@translations/languageDetector';
 
 test.describe('Visibility Tests for "Välimus ja käitumine"/"Appearance and Behaviour" Page', async () => {
     let translation;
