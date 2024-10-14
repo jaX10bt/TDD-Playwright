c5a5831e9e1b1a43561c721043dba3e5368bacbe
jaX10bt <132996313+jaX10bt@users.noreply.github.com>
2024-10-14 15:42:34 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,11 +4,11 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
 
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  //workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'always' }], // Generates the HTML report
@@ -16,7 +16,7 @@ module.exports = defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    storageState: 'tests/admin/.auth/user.json',
+    //storageState: 'tests/admin/.auth/user.json',
     video: 'on',
     screenshot: 'only-on-failure',
     trace: 'on',
@@ -31,7 +31,7 @@ module.exports = defineConfig({
               storageState: 'tests/admin/.auth/user.json',
       },
       dependencies: ['setup'],
+      workers: 1,
     },
   ],
 });
-
